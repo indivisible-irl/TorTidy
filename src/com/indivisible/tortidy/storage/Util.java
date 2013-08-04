704e85341eb87ffbfef5db8f0f754e0efd662440
indivisible-irl <mbspare@gmail.com>
2013-08-04 14:31:31 +0100

    util: added bool/int conversion

--- src/com/indivisible/tortidy/storage/Util.java
+++ src/com/indivisible/tortidy/storage/Util.java
@@ -3,9 +3,11 @@ import java.io.*;
 import android.content.*;
 import java.util.*;
 import com.indivisible.tortidy.database.*;
+import android.util.*;
 
 public class Util
 {
+	private static final String TAG = "com.indivisible.tortidy";
 	
 	/** get a label name from torrent's folder location **/
 	public static String getLabelFromLocation(File tor, File root) {
@@ -35,4 +37,22 @@ public class Util
 //	public static boolean moveFile(File tor, File destination) {
 //		tor.re
 //	}
+	
+	
+	public static int boolToInt(boolean bool) {
+		if (bool)
+			return 1;
+		return 0;
+	}
+	
+	public static boolean intToBool(int i) {
+		if (i == 0)
+			return false;
+		else if (i == 1)
+			return true;
+		else {
+			Log.e(TAG, "intToBool invalid int: " +i);
+			return false;
+		}
+	}
 }
