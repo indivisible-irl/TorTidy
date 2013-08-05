e9d02c1bbda85f2d88a33f90a9ab5df7b2a2d958
indivisible-irl <mbspare@gmail.com>
2013-08-05 18:32:59 +0100

    ui: labelManager add buttons

--- src/com/indivisible/tortidy/ui/LabelManagerActivity.java
+++ src/com/indivisible/tortidy/ui/LabelManagerActivity.java
@@ -6,18 +6,30 @@ import com.indivisible.tortidy.*;
 import com.indivisible.tortidy.database.*;
 import java.util.*;
 import android.widget.*;
+import android.view.*;
 
 public class LabelManagerActivity extends ListActivity
 {
 	private static final String TAG = "com.indivisible.tortidy";
 	
+	private Button bAddOrConfirm;
+	private Button bDeleteOrCancel;
+	
 	private LabelsDataSource labels;
+	private boolean isDeleteMode;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.label_manager);
 		
+		isDeleteMode = false;
+		
+		// buttons:
+		bAddOrConfirm   = (Button) findViewById(R.id.bLabelAddOrConfirm);
+		bDeleteOrCancel = (Button) findViewById(R.id.bLabelDeleteOrCancel);
+		
+		// labels:
 		labels = new LabelsDataSource(this.getApplicationContext());
 		labels.openWriteable();
 		
@@ -39,6 +51,31 @@ public class LabelManagerActivity extends ListActivity
 	protected void onPause() {
 		labels.close();
 		super.onPause();
+	}
+	
+	//// onClick (use xml call or listeners?)
+	public void onClick(View view) {
+		
+		if(isDeleteMode) {
+			switch (view.getId()) {
+				case R.id.bLabelAddOrConfirm:
+					break;
+				case R.id.bLabelDeleteOrCancel:
+					isDeleteMode = false;
+					break;
+			}
+		}
+		else {
+			switch (view.getId()) {
+				case R.id.bLabelAddOrConfirm:
+					break;
+			
+				case R.id.bLabelDeleteOrCancel:
+					isDeleteMode = true;
+					break;
+			}
+		}
+		
 	}
 	
 }
